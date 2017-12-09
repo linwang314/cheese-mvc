d256bf926fe6c1a1275889d6813851576dd5cf0c
linwang929 <lin.wang@go.wustl.edu>
2017-12-09 11:18:55 -0600

    completed Class 3.8 studio to add User class (except for rendering error message when password validation fails)

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/UserController.java
@@ -0,0 +1,31 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.launchcode.cheesemvc.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Add User");
+
+        return "user/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute User newUser, String verify) {
+        if (newUser.getPassword().equals(verify)) {
+            model.addAttribute("welcome", "Welcome, " + newUser.getUsername());
+            return "user/index";
+        } else {
+            return "user/add";
+        }
+    }
+
+}
