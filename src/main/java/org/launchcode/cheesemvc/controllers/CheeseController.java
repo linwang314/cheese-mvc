e7fbd4d91870111d959e0749d24a73b6893dfda7
linwang929 <lin.wang@go.wustl.edu>
2017-12-08 22:11:08 -0600

    followed Class 3.8 Part 2 to add unique identifier and model binding

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,27 +1,25 @@
 package org.launchcode.cheesemvc.controllers;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<Cheese> cheeses = new ArrayList<>();
-
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
 
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "My Cheeses");
 
         return "cheese/index";
@@ -34,36 +32,34 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+    public String processAddCheeseForm(@ModelAttribute Cheese newCheese) {
 
+        CheeseData.add(newCheese);
+        return "redirect:";
+
+        /** validation before model binding
+         *
         if (!cheeseName.isEmpty()) {
-            Cheese aNewCheese = new Cheese(cheeseName, cheeseDescription);
-            cheeses.add(aNewCheese);
+            Cheese newCheese = new Cheese(cheeseName, cheeseDescription);
+            CheeseData.add(newCheese);
             return "redirect:";
         } else {return "redirect:add";}
+
+         **/
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.GET)
     public String displayRemoveCheeseForm(Model model) {
         model.addAttribute("title", "Remove Cheese");
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         return "cheese/remove";
     }
 
     @RequestMapping(value = "remove", method = RequestMethod.POST)
-    public String processRemoveCheeseForm(@RequestParam ArrayList<Cheese> cheeseRemoved) {
-        /* *
-         * use nested loop to find out the cheeses to be removed from the original list
-         * cheeses.removeAll(cheeseRemoved) or cheeses.remove() did NOT work
-         * the elements in cheeseRemoved are different from those in cheeses even if they have the same value
-         * */
+    public String processRemoveCheeseForm(@RequestParam int[] cheeseIds) {
 
-        for (Cheese eachCheese : cheeseRemoved) {
-            int i = 0;
-            while (cheeses.get(0).getName().equals(eachCheese.getName())) {
-                i++;
-            }
-            cheeses.remove(i);
+        for (int cheeseId : cheeseIds) {
+            CheeseData.remove(cheeseId);
         }
 
         return "redirect:";
